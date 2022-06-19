dfab26a2ba7c0864fff34d097d86d49453bc22fc
saranyavivekanandan <saranya010694@gmail.com>
2022-06-18 20:58:49 -0500

    update2 (#2)

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("DevClass");
-//         Checking from stream2learn 
+//         Checking from stream2learn - part2
     }
 }
