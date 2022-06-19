8f64b9422c7a1706ed55b545d867b022c940aac6
Saranya-Vivekananda <saranyav010694@gmail.com>
2022-06-18 20:51:34 -0500

    new Change 2

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,5 +2,6 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("DevClass");
         System.out.println("new Change");
+        System.out.println("change from BranchNew ");
     }
 }
