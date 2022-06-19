300b9db4770b866b9bd011a95a50f1ba9e14c631
Saranya-Vivekananda <saranyav010694@gmail.com>
2022-06-18 20:53:32 -0500

    deleted update

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,7 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("DevClass");
-        System.out.println("new Change");
-        System.out.println("change from BranchNew ");
+        System.out.println("deleted all changes ");
     }
 }
